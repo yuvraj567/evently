c34a0f7c84f5d985a0f7c5b4f1a3c41246987f4a
Yuvraj <yuvraj8847.11@gmail.com>
2024-06-21 17:58:17 +0530

    another commit

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,7 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 
-import { connectToDatabase } from "@/lib/database";
+import { connectToDatabase } from "@/lib/database/index";
 import Event from "@/lib/database/models/event.model";
 import Order from "@/lib/database/models/order.model";
 import User from "@/lib/database/models/user.model";
